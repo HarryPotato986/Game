6ddccbea777310a821ccb3b6cdd4c2526203916c
Gavin <gdude986@gmail.com>
2023-12-07 10:10:23 -0500

    working on ranged weapon and projectiles

--- classes/Item/RangedWeaponItem.py
+++ classes/Item/RangedWeaponItem.py
@@ -5,7 +5,14 @@ class RangedWeaponItem(WeaponItem):
 
     def __init__(self, ResourceLocation, texture, itemName, damage, weaponRange, knockback, weaponCooldown):
         super().__init__(ResourceLocation, texture, itemName, damage, weaponRange, knockback, weaponCooldown)
+        self.activeProjectiles = []
 
+
+    def ticker(self):
+        if self.weaponCooldownTimer > 0:
+            self.weaponCooldownTimer -= 1
+        for projectile in self.activeProjectiles:
+            projectile.ticker()
 
     def attack(self, userCollisionBox, userX, userY, facing):
         if self.weaponCooldownTimer == 0:
---
+++ classes/Projectile.py
@@ -0,0 +1,13 @@
+
+
+class Projectile:
+
+    def __init__(self, speed, damage, direction, projectileRange):
+        self.speed = speed
+        self.damage = damage
+        self.direction = direction
+        self.projectileRange = projectileRange
+
+
+    def ticker(self):
+        pass
