6b2f484b678de732d29bd036e4305eef24ff53ea
HarryPotato986 <gdude986@gmail.com>
2023-12-20 23:09:59 -0500

    tiny change

--- Init/RoomInit.py
+++ Init/RoomInit.py
@@ -1,4 +1,4 @@
 from classes.Rooms.BaseRoom import BaseRoom
 
 
-testRoom = BaseRoom("assets/room_textures", "Room number 1.png", ((100, 800, 100, 100), (100, 00, 100, 100)))
+testRoom = BaseRoom("assets/room_textures", "Room number 1.png", ((100, 800, 100, 100), (100, 500, 100, 100)))
